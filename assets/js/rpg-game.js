c41cab8b25ead06f648a435b1f8d8850e7313e8f
Greg Church <greggypc@users.noreply.github.com>
2017-11-21 18:15:30 -0600

    setup games files

---
+++ assets/js/rpg-game.js
@@ -0,0 +1,14 @@
+// RPG Game
+
+$(document).ready(function() {
+
+	var player;
+	var opponent;
+	var healthPoints = 0;
+	var attackPoints = 0;
+	var counterAttackPoints = 0;
+	
+
+
+
+}); //end document.ready
