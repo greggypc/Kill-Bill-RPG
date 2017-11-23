ecd1b157a32f7a0e821adbe361a1144f68e772d7
Greg Church <greggypc@users.noreply.github.com>
2017-11-23 16:25:22 -0600

    1st step complete - choose player and opponent

--- assets/js/rpg-game.js
+++ assets/js/rpg-game.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 
-	// var player;
+	
 	// var opponent;
 	// var healthPoints = 0;
 	// var attackPoints = 0;
@@ -16,7 +16,8 @@ $(document).ready(function() {
         name: "Beatrix Kiddo",
         healthPoints: 129,
         attackPower: 4,
-        counterAttackPower: 7
+        counterAttackPower: 7,
+        role: "enemy"
       }, {
         name: "Elle Driver",
         healthPoints: 133,
@@ -34,18 +35,79 @@ $(document).ready(function() {
         counterAttackPower: 7
       }]
     };
+    var role =  null;
+    var fighterImg1 = "../week-4-rpg-game/assets/images/kill-beatrix-kiddo.jpg";
+    var fighterImg2 = "../week-4-rpg-game/assets/images/kill-elle-driver.jpg";
+    var fighterImg3 = "../week-4-rpg-game/assets/images/kill-o-ren-ishii.jpg";
+    var fighterImg4 = "../week-4-rpg-game/assets/images/kill-vernita-green.jpg";
 
 	//Initialize Game
 	//========================================
 	function layoutGame() {
-		$("#presentFourFighters").html('<img id="presentFourFighters" src="../week-4-rpg-game/assets/images/kill-beatrix-kiddo.jpg" />');
-		$("#presentFourFighters").append('<img id="presentFourFighters" src="../week-4-rpg-game/assets/images/kill-elle-driver.jpg" />');
-		$("#presentFourFighters").append('<img id="presentFourFighters" src="../week-4-rpg-game/assets/images/kill-o-ren-ishii.jpg" />');
-		$("#presentFourFighters").append('<img id="presentFourFighters" src="../week-4-rpg-game/assets/images/kill-vernita-green.jpg" />');
+		$("#fighterImg1").html('<img value="characters.fighter[0]" id="fighterImg" src=' + fighterImg1 + ' />');
+		$("#fighterImg2").html('<img value="characters.fighter[1]" id="fighterImg" src=' + fighterImg2 + ' />');
+		$("#fighterImg3").html('<img value="characters.fighter[2]" id="fighterImg" src=' + fighterImg3 + ' />');
+		$("#fighterImg4").html('<img value="characters.fighter[3]" id="fighterImg" src=' + fighterImg4 + ' />');
 	}
 
-	$("#buttAppendp").click(function() {
-    	$("p").append("<b>Appended text to ALL p's</b>");
+	$("#fighterImg1").click(function() {
+    	if (role === null) {
+    		$("#playerChosen").html('<img id="fighterImg" src=' + fighterImg1 + ' />');
+    		$(".playerName").html('<p>Beatrix Kiddo</p>');
+    		$("#fighterImg1").hide();
+    		role = "enemy";
+    		console.log("just picked our player");
+    	} else {
+    		$("#enemyChosen").html('<img id="fighterImg" src=' + fighterImg1 + ' />');
+    		$(".enemyName").html('<p>Beatrix Kiddo</p>');
+    		$("#fighterImg1").hide();
+    		console.log("just picked our enemy");
+    	}
+    });
+
+    $("#fighterImg2").click(function() {
+    	if (role === null) {
+    		$("#playerChosen").html('<img id="fighterImg" src=' + fighterImg2 + ' />');
+    		$(".playerName").html('<p>Elle Driver</p>');
+    		$("#fighterImg2").hide();
+    		role = "enemy";
+    		console.log("just picked our player");
+    	} else {
+    		$("#enemyChosen").html('<img id="fighterImg" src=' + fighterImg2 + ' />');
+    		$(".enemyName").html('<p>Elle Driver</p>');
+    		$("#fighterImg2").hide();
+    		console.log("just picked our enemy");
+    	}
+    });
+
+    $("#fighterImg3").click(function() {
+    	if (role === null) {
+    		$("#playerChosen").html('<img id="fighterImg" src=' + fighterImg3 + ' />');
+    		$(".playerName").html('<p>O Ren Ishii</p>');
+    		$("#fighterImg3").hide();
+    		role = "enemy";
+    		console.log("just picked our player");
+    	} else {
+    		$("#enemyChosen").html('<img id="fighterImg" src=' + fighterImg3 + ' />');
+    		$(".enemyName").html('<p>O Ren Ishii</p>');
+    		$("#fighterImg3").hide();
+    		console.log("just picked our enemy");
+    	}
+    });
+
+    $("#fighterImg4").click(function() {
+    	if (role === null) {
+    		$("#playerChosen").html('<img id="fighterImg" src=' + fighterImg4 + ' />');
+    		$(".playerName").html('<p>Vernita Green</p>');
+    		$("#fighterImg4").hide();
+    		role = "enemy";
+    		console.log("just picked our player");
+    	} else {
+    		$("#enemyChosen").html('<img id="fighterImg" src=' + fighterImg4 + ' />');
+    		$(".enemyName").html('<p>Vernita Green</p>');
+    		$("#fighterImg4").hide();
+    		console.log("just picked our enemy");
+    	}
     });
 
 
